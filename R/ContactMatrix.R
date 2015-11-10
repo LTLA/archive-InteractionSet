7b86d585c78851dff36924dbf6e7055c8bc753b9
Aaron Lun <alun@wehi.edu.au>
2015-11-10 18:09:14 +0000

    Switched representation of ContactMatrix to have a matrix slot.

--- R/ContactMatrix.R
+++ R/ContactMatrix.R
@@ -2,8 +2,8 @@
 # Defines the ContactMatrix class.
 
 setClass("ContactMatrix", 
-    contains="matrix",
     slots=list(
+        matrix="matrix", 
         anchor1="integer",
         anchor2="integer",
         regions="GRanges"
@@ -11,47 +11,46 @@ setClass("ContactMatrix",
 )
 
 setValidity("ContactMatrix", function(object) {
-    if (nrow(object)!=length(object@anchor1)) { 
-        return("'assays' nrow must be equal to length of 'anchor1'")
-    }
-    if (ncol(object)!=length(object@anchor2)) {
-        return("'assays' ncol must be equal to length of 'anchor2'")
-    }
-
-    if (is.unsorted(object@regions)) { 
+    if (is.unsorted(object@regions)) {
         return("'regions' should be sorted")
     }
-    msg <- .check_inputs(object@anchor1, object@anchor2, object@regions)
+    msg <- .check_inputs(object@anchor1, object@anchor2, object@regions, same.length=FALSE)
     if (is.character(msg)) { return(msg) }
-   
+
+    if (nrow(object@matrix)!=length(object@anchor1)) { 
+        return("'matrix' nrow must be equal to length of 'anchor1'")
+    }
+    if (ncol(object@matrix)!=length(object@anchor2)) {
+        return("'matrix' ncol must be equal to length of 'anchor2'")
+    }
     return(TRUE)
 }) 
 
 setMethod("show", signature("ContactMatrix"), function(object) {
     cat("class:", class(object), "\n")
-    cat("dim:", dim(object), "\n")
+    cat("dim:", dim(object@matrix), "\n")
     cat(sprintf("regions: %i\n", length(object@regions)))
 })
 
 ##############################################
 # Constructor:
 
-.new_ContactMatrix <- function(mat, anchor1, anchor2, regions) {
+.new_ContactMatrix <- function(matrix, anchor1, anchor2, regions) {
     anchor1 <- as.integer(anchor1)
     anchor2 <- as.integer(anchor2)
     
-    msg <- .check_inputs(anchor1, anchor2, regions)
+    msg <- .check_inputs(anchor1, anchor2, regions, same.length=FALSE)
     if (is.character(msg)) { stop(msg) }
-    out <- .resort_regions(anchor1, anchor2, regions)
+    out <- .resort_regions(anchor1, anchor2, regions, enforce.order=FALSE)
 
-    new("ContactMatrix", mat, anchor1=out$anchor1, anchor2=out$anchor2, regions=out$regions)
+    new("ContactMatrix", matrix=matrix, anchor1=out$anchor1, anchor2=out$anchor2, regions=out$regions)
 }
 
-setGeneric("ContactMatrix", function(mat, anchor1, anchor2, ...) { standardGeneric("ContactMatrix") })
+setGeneric("ContactMatrix", function(matrix, anchor1, anchor2, ...) { standardGeneric("ContactMatrix") })
 setMethod("ContactMatrix", c("ANY", "numeric", "numeric"), .new_ContactMatrix)
 
 setMethod("ContactMatrix", c("ANY", "GRanges", "GRanges"), 
-    function(mat, anchor1, anchor2, regions) { 
+    function(matrix, anchor1, anchor2, regions) { 
 
         if (missing(regions)) { 
             collated <- .collate_GRanges(anchor1, anchor2)
@@ -66,9 +65,39 @@ setMethod("ContactMatrix", c("ANY", "GRanges", "GRanges"),
             }
         }
         
-        .new_ContactMatrix(mat, anchor1, anchor2, regions)
+        .new_ContactMatrix(matrix, anchor1, anchor2, regions)
     }
 )
 
 ##############################################
+# Subsetting
+
+setMethod("[", c("ContactMatrix", "ANY", "ANY"), function(x, i, j, ..., drop=TRUE) {
+    if (!missing(i)) { 
+        x@anchor1 <- x@anchor1
+    }
+    if (!missing(j)) {
+        x@anchor2 <- x@anchor2[j]
+    }
+    x@matrix <- x@matrix[i,j,drop=FALSE]
+    return(x)
+}) 
+
+setMethod("subset", "ContactMatrix", function(x, i, j) {
+    x[i,j]
+})
+
+##############################################
+# Other getters
+
+setMethod("as.matrix", "ContactMatrix", function(x) {
+    return(x@matrix)
+}) 
+
+##############################################
 # End
+
+
+# Testing:
+# cm <- InteractionSet:::ContactMatrix(matrix(0, 4, 4), 1:4, 1:4, GRanges("chr1", IRanges(1:4, 1:4)))
+
